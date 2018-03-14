2e2f52d9a0de43755a06e84b44d136cea0f1eeb9
rgm19 <gmruben1995@gmail.com>
2018-03-14 12:48:53 +0100

    1.1 imagens

---
+++ app/src/main/java/com/example/ruben/dogsapp/ActivityDetalles.java
@@ -0,0 +1,44 @@
+package com.example.ruben.dogsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+public class ActivityDetalles extends AppCompatActivity {
+
+    static Dog dogie = new Dog();
+    ImageView img_detalles;
+    TextView descripcion , comportamiento, carac;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        img_detalles=findViewById(R.id.img_detalles);
+        Picasso.with(this).load(dogie.getUrl()).placeholder(R.drawable.placeholder).error(R.drawable.placeholder).into(img_detalles);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(dogie.getName());
+        }
+
+        descripcion = findViewById(R.id.detalles_descripcion);
+        descripcion.setText(dogie.getDescripcion());
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/ruben/dogsapp/InicioFragment.java
@@ -0,0 +1,59 @@
+package com.example.ruben.dogsapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import static com.example.ruben.dogsapp.ActivityDetalles.dogie;
+
+
+public class InicioFragment extends Fragment {
+
+    final static  String DB_URL= "https://dogsappdam.firebaseio.com/";
+    ListView listView;
+    FirebaseClient firebaseClient;
+    View view;
+
+    public InicioFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_inicio,container,false);
+
+
+        listView = (ListView) view.findViewById(R.id.listview);
+        listView.setClickable(true);
+        firebaseClient = new FirebaseClient(getContext(), DB_URL, listView);
+        firebaseClient.refreshdata();
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+
+                //Object o = listView.getItemAtPosition(position);
+                // Realiza lo que deseas, al recibir clic en el elemento de tu listView determinado por su posicion.
+                Intent intent = new Intent(view.getContext(), ActivityDetalles.class);
+                dogie = (Dog) listView.getItemAtPosition(position);
+                startActivity(intent);
+
+            }
+        });
+
+        return view;
+    }
+}
